32ce97df2ceefdb8bef3d75fe68abe9a3f3d2deb
Bùi Văn Hà <ha12c3tnhvt@gmail.com>
2019-10-02 10:24:02 +0700

    Thư mục chứa file Word yêu cầu và file code project của Đồ Án
    Nhóm thực hiện :
    17110129 : Bùi Văn Hà
    17110209 : Lư Mạnh Quân

---
+++ Đồ_Án_1/Đồ_Án_1/Thao_tác_với_file.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Đồ_Án_1
+{
+    partial class Thao_tác_với_file
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnAddAfter = new System.Windows.Forms.Button();
+            this.BtnResetReview = new System.Windows.Forms.Button();
+            this.BtnFind = new System.Windows.Forms.Button();
+            this.BtnRemoveLast = new System.Windows.Forms.Button();
+            this.BtnRemoveFirst = new System.Windows.Forms.Button();
+            this.BtnRemove = new System.Windows.Forms.Button();
+            this.BtnAddLast = new System.Windows.Forms.Button();
+            this.BtnAddBefore = new System.Windows.Forms.Button();
+            this.TextBoxReview = new System.Windows.Forms.TextBox();
+            this.BtnAddFirst = new System.Windows.Forms.Button();
+            this.BtnXem = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnAddAfter
+            // 
+            this.BtnAddAfter.Location = new System.Drawing.Point(562, 22);
+            this.BtnAddAfter.Name = "BtnAddAfter";
+            this.BtnAddAfter.Size = new System.Drawing.Size(105, 41);
+            this.BtnAddAfter.TabIndex = 27;
+            this.BtnAddAfter.Text = "Add After";
+            this.BtnAddAfter.UseVisualStyleBackColor = true;
+            this.BtnAddAfter.Click += new System.EventHandler(this.BtnAddAfter_Click);
+            // 
+            // BtnResetReview
+            // 
+            this.BtnResetReview.Location = new System.Drawing.Point(710, 390);
+            this.BtnResetReview.Name = "BtnResetReview";
+            this.BtnResetReview.Size = new System.Drawing.Size(121, 137);
+            this.BtnResetReview.TabIndex = 26;
+            this.BtnResetReview.Text = "Reset Review";
+            this.BtnResetReview.UseVisualStyleBackColor = true;
+            this.BtnResetReview.Click += new System.EventHandler(this.BtnResetReview_Click);
+            // 
+            // BtnFind
+            // 
+            this.BtnFind.Location = new System.Drawing.Point(872, 22);
+            this.BtnFind.Name = "BtnFind";
+            this.BtnFind.Size = new System.Drawing.Size(109, 38);
+            this.BtnFind.TabIndex = 25;
+            this.BtnFind.Text = "Find Node";
+            this.BtnFind.UseVisualStyleBackColor = true;
+            this.BtnFind.Click += new System.EventHandler(this.BtnFind_Click);
+            // 
+            // BtnRemoveLast
+            // 
+            this.BtnRemoveLast.Location = new System.Drawing.Point(710, 235);
+            this.BtnRemoveLast.Name = "BtnRemoveLast";
+            this.BtnRemoveLast.Size = new System.Drawing.Size(113, 36);
+            this.BtnRemoveLast.TabIndex = 24;
+            this.BtnRemoveLast.Text = "Remove Last";
+            this.BtnRemoveLast.UseVisualStyleBackColor = true;
+            this.BtnRemoveLast.Click += new System.EventHandler(this.BtnRemoveLast_Click);
+            // 
+            // BtnRemoveFirst
+            // 
+            this.BtnRemoveFirst.Location = new System.Drawing.Point(710, 161);
+            this.BtnRemoveFirst.Name = "BtnRemoveFirst";
+            this.BtnRemoveFirst.Size = new System.Drawing.Size(114, 34);
+            this.BtnRemoveFirst.TabIndex = 23;
+            this.BtnRemoveFirst.Text = "Remove First";
+            this.BtnRemoveFirst.UseVisualStyleBackColor = true;
+            this.BtnRemoveFirst.Click += new System.EventHandler(this.BtnRemoveFirst_Click);
+            // 
+            // BtnRemove
+            // 
+            this.BtnRemove.Location = new System.Drawing.Point(710, 91);
+            this.BtnRemove.Name = "BtnRemove";
+            this.BtnRemove.Size = new System.Drawing.Size(114, 35);
+            this.BtnRemove.TabIndex = 22;
+            this.BtnRemove.Text = "Remove Node";
+            this.BtnRemove.UseVisualStyleBackColor = true;
+            this.BtnRemove.Click += new System.EventHandler(this.BtnRemove_Click);
+            // 
+            // BtnAddLast
+            // 
+            this.BtnAddLast.Location = new System.Drawing.Point(710, 22);
+            this.BtnAddLast.Name = "BtnAddLast";
+            this.BtnAddLast.Size = new System.Drawing.Size(115, 39);
+            this.BtnAddLast.TabIndex = 21;
+            this.BtnAddLast.Text = "Add Last";
+            this.BtnAddLast.UseVisualStyleBackColor = true;
+            this.BtnAddLast.Click += new System.EventHandler(this.BtnAddLast_Click);
+            // 
+            // BtnAddBefore
+            // 
+            this.BtnAddBefore.Location = new System.Drawing.Point(407, 22);
+            this.BtnAddBefore.Name = "BtnAddBefore";
+            this.BtnAddBefore.Size = new System.Drawing.Size(110, 41);
+            this.BtnAddBefore.TabIndex = 20;
+            this.BtnAddBefore.Text = "Add Before";
+            this.BtnAddBefore.UseVisualStyleBackColor = true;
+            this.BtnAddBefore.Click += new System.EventHandler(this.BtnAddBefore_Click);
+            // 
+            // TextBoxReview
+            // 
+            this.TextBoxReview.Location = new System.Drawing.Point(45, 98);
+            this.TextBoxReview.Multiline = true;
+            this.TextBoxReview.Name = "TextBoxReview";
+            this.TextBoxReview.Size = new System.Drawing.Size(622, 429);
+            this.TextBoxReview.TabIndex = 19;
+            // 
+            // BtnAddFirst
+            // 
+            this.BtnAddFirst.Location = new System.Drawing.Point(249, 22);
+            this.BtnAddFirst.Name = "BtnAddFirst";
+            this.BtnAddFirst.Size = new System.Drawing.Size(118, 42);
+            this.BtnAddFirst.TabIndex = 18;
+            this.BtnAddFirst.Text = "Add First";
+            this.BtnAddFirst.UseVisualStyleBackColor = true;
+            this.BtnAddFirst.Click += new System.EventHandler(this.BtnAddFirst_Click);
+            // 
+            // BtnXem
+            // 
+            this.BtnXem.Location = new System.Drawing.Point(45, 22);
+            this.BtnXem.Name = "BtnXem";
+            this.BtnXem.Size = new System.Drawing.Size(160, 42);
+            this.BtnXem.TabIndex = 17;
+            this.BtnXem.Text = "Mở file và xem";
+            this.BtnXem.UseVisualStyleBackColor = true;
+            this.BtnXem.Click += new System.EventHandler(this.BtnXem_Click);
+            // 
+            // Thao_tác_với_file
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 549);
+            this.Controls.Add(this.BtnAddAfter);
+            this.Controls.Add(this.BtnResetReview);
+            this.Controls.Add(this.BtnFind);
+            this.Controls.Add(this.BtnRemoveLast);
+            this.Controls.Add(this.BtnRemoveFirst);
+            this.Controls.Add(this.BtnRemove);
+            this.Controls.Add(this.BtnAddLast);
+            this.Controls.Add(this.BtnAddBefore);
+            this.Controls.Add(this.TextBoxReview);
+            this.Controls.Add(this.BtnAddFirst);
+            this.Controls.Add(this.BtnXem);
+            this.Name = "Thao_tác_với_file";
+            this.Text = "Thao tác với file";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnAddAfter;
+        private System.Windows.Forms.Button BtnResetReview;
+        private System.Windows.Forms.Button BtnFind;
+        private System.Windows.Forms.Button BtnRemoveLast;
+        private System.Windows.Forms.Button BtnRemoveFirst;
+        private System.Windows.Forms.Button BtnRemove;
+        private System.Windows.Forms.Button BtnAddLast;
+        private System.Windows.Forms.Button BtnAddBefore;
+        private System.Windows.Forms.TextBox TextBoxReview;
+        private System.Windows.Forms.Button BtnAddFirst;
+        private System.Windows.Forms.Button BtnXem;
+    }
+}
+
---
+++ Đồ_Án_1/Đồ_Án_1/Thao_tác_với_file.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Đồ_Án_1
+{
+    public partial class Thao_tác_với_file : Form
+    {
+        public Thao_tác_với_file()
+        {
+            InitializeComponent();
+        }
+        public class Node
+        {
+            public int data;
+            public Node next;
+            public Node(int i)
+            {
+                data = i;
+                next = null;
+            }
+        }
+        public class MyList
+        {
+            public Node headNode;
+            public MyList()
+            {
+                headNode = null;
+            }
+        }
+        XemFile xf = new XemFile();
+        public void Linkedlist()
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Text Documents(*.txt)|*.txt", ValidateNames = true, Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string[] lines = System.IO.File.ReadAllLines(ofd.FileName);
+                    LinkedList<string> lists = new LinkedList<string>(lines);
+                    /*
+                    foreach (string s in lines)
+                    {
+                        lists.AddLast(s);   //them dan ve sau
+                    }*/
+                    Text = String.Concat(lists); //Chuyen 'lists' tu dang string[] --> string
+                    xf.TextBoxSach.Text = Text;
+                }
+            }
+        }
+
+        private void BtnXem_Click(object sender, EventArgs e)
+        {
+            Linkedlist();
+            xf.ShowDialog();
+        }
+        //Them, xoa theo thu tu:
+        private void BtnAddFirst_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Text Documents(*.txt)|*.txt", ValidateNames = true, Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string[] lines = System.IO.File.ReadAllLines(ofd.FileName);
+                    LinkedList<string> lists = new LinkedList<string>(lines);
+                    /*foreach (string s in lines)
+                    {
+                        lists.AddLast(s);   //them dan ve sau
+                    }*/
+                    Text = String.Concat(lists); //Chuyen 'lists' tu dang string[] --> string
+                    xf.TextBoxSach.Text = Text;
+                    //Add before
+                    LinkedListNode<string> nodebefore1 = lists.Find("1001      Cấu Trúc Dữ Liệu Và Giải Thuật     85000     1 ");
+                    //var nodebefore = lists.AddLast("5013      Sức bền vật liệu 2                 78000     5 ");
+                    lists.AddBefore(nodebefore1, "1000      Lập trình trên GPU                 150000    5 ");
+                    foreach (var x in lists)
+                    {
+                        TextBoxReview.Text = x;
+                    }
+                    Text = String.Join(" ", lists);
+                    TextBoxReview.Text = String.Join(" ", lists);
+                    System.IO.File.WriteAllText(@"C:\Users\admin\Desktop\Sachupdate.txt", Text);
+                }
+            }
+        }
+
+        private void BtnAddBefore_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Text Documents(*.txt)|*.txt", ValidateNames = true, Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string[] lines = System.IO.File.ReadAllLines(ofd.FileName);
+                    LinkedList<string> lists = new LinkedList<string>(lines);
+                    /*foreach (string s in lines)
+                    {
+                        lists.AddLast(s);   //them dan ve sau
+                    }*/
+                    Text = String.Concat(lists); //Chuyen 'lists' tu dang string[] --> string
+                    xf.TextBoxSach.Text = Text;
+                    //Add before
+                    LinkedListNode<string> nodebefore1 = lists.Find("5010      Tin học cơ sở                      88000     5 ");
+                    //var nodebefore = lists.AddLast("5013      Sức bền vật liệu 2                 78000     5 ");
+                    lists.AddBefore(nodebefore1, "5013      Sức bền vật liệu 2                 78000     5 ");
+                    foreach (var x in lists)
+                    {
+                        TextBoxReview.Text = x;
+                    }
+                    Text = String.Join(" ", lists);
+                    TextBoxReview.Text = String.Join(" ", lists);
+                    System.IO.File.WriteAllText(@"C:\Users\admin\Desktop\Sachupdate.txt", Text);
+                }
+            }
+        }
+
+        private void BtnAddAfter_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Text Documents(*.txt)|*.txt", ValidateNames = true, Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string[] lines = System.IO.File.ReadAllLines(ofd.FileName);
+                    LinkedList<string> lists = new LinkedList<string>(lines);
+                    /*foreach (string s in lines)
+                    {
+                        lists.AddLast(s);   //them dan ve sau
+                    }*/
+                    Text = String.Concat(lists); //Chuyen 'lists' tu dang string[] --> string
+                    xf.TextBoxSach.Text = Text;
+                    //Add before
+                    LinkedListNode<string> nodebefore1 = lists.Find("5010      Tin học cơ sở                      88000     5 ");
+                    //var nodebefore = lists.AddLast("5013      Sức bền vật liệu 2                 78000     5 ");
+                    lists.AddAfter(nodebefore1, "5013      Sức bền vật liệu 2                 78000     5 ");
+                    foreach (var x in lists)
+                    {
+                        TextBoxReview.Text = x;
+                    }
+                    Text = String.Join(" ", lists);
+                    TextBoxReview.Text = String.Join(" ", lists);
+                    System.IO.File.WriteAllText(@"C:\Users\admin\Desktop\Sachupdate.txt", Text);
+                }
+            }
+        }
+
+        private void BtnAddLast_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Text Documents(*.txt)|*.txt", ValidateNames = true, Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string[] lines = System.IO.File.ReadAllLines(ofd.FileName);
+                    LinkedList<string> lists = new LinkedList<string>(lines);
+                    /*foreach (string s in lines)
+                    {
+                        lists.AddLast(s);   //them dan ve sau
+                    }*/
+                    Text = String.Concat(lists); //Chuyen 'lists' tu dang string[] --> string
+                    xf.TextBoxSach.Text = Text;
+                    lists.AddLast("5013      Sức bền vật liệu 2                 78000     5 ");
+                    foreach (var x in lists)
+                    {
+                        TextBoxReview.Text = x;
+                    }
+                    Text = String.Join(" ", lists);
+                    TextBoxReview.Text = String.Join(" ", lists);
+                    System.IO.File.WriteAllText(@"C:\Users\admin\Desktop\Sachupdate.txt", Text);
+                }
+            }
+        }
+
+        private void BtnRemove_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Text Documents(*.txt)|*.txt", ValidateNames = true, Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string[] lines = System.IO.File.ReadAllLines(ofd.FileName);
+                    LinkedList<string> lists = new LinkedList<string>(lines);
+                    /*foreach (string s in lines)
+                    {
+                        lists.AddLast(s);   //them dan ve sau
+                    }*/
+                    Text = String.Concat(lists); //Chuyen 'lists' tu dang string[] --> string
+                    xf.TextBoxSach.Text = Text;
+                    lists.Remove("4006      Trải nghiệm văn hóa ẩm thực        160000    4 ");
+                    foreach (var x in lists)
+                    {
+                        TextBoxReview.Text = x;
+                    }
+                    Text = String.Join(" ", lists);
+                    TextBoxReview.Text = String.Join(" ", lists);
+                    System.IO.File.WriteAllText(@"C:\Users\admin\Desktop\Sachupdate.txt", Text);
+                }
+            }
+        }
+
+        private void BtnRemoveFirst_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Text Documents(*.txt)|*.txt", ValidateNames = true, Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string[] lines = System.IO.File.ReadAllLines(ofd.FileName);
+                    LinkedList<string> lists = new LinkedList<string>(lines);
+                    /*foreach (string s in lines)
+                    {
+                        lists.AddLast(s);   //them dan ve sau
+                    }*/
+                    Text = String.Concat(lists); //Chuyen 'lists' tu dang string[] --> string
+                    xf.TextBoxSach.Text = Text;
+                    lists.Remove("1001      Cấu Trúc Dữ Liệu Và Giải Thuật     85000     1 ");
+                    foreach (var x in lists)
+                    {
+                        TextBoxReview.Text = x;
+                    }
+                    Text = String.Join(" ", lists);
+                    TextBoxReview.Text = String.Join(" ", lists);
+                    System.IO.File.WriteAllText(@"C:\Users\admin\Desktop\Sachupdate.txt", Text);
+                }
+            }
+        }
+
+        private void BtnRemoveLast_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Text Documents(*.txt)|*.txt", ValidateNames = true, Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string[] lines = System.IO.File.ReadAllLines(ofd.FileName);
+                    LinkedList<string> lists = new LinkedList<string>(lines);
+                    /*foreach (string s in lines)
+                    {
+                        lists.AddLast(s);   //them dan ve sau
+                    }*/
+                    Text = String.Concat(lists); //Chuyen 'lists' tu dang string[] --> string
+                    xf.TextBoxSach.Text = Text;
+                    lists.RemoveLast();
+                    foreach (var x in lists)
+                    {
+                        TextBoxReview.Text = x;
+                    }
+                    Text = String.Join(" ", lists);
+                    TextBoxReview.Text = String.Join(" ", lists);
+                    System.IO.File.WriteAllText(@"C:\Users\admin\Desktop\Sachupdate.txt", Text);
+                }
+            }
+        }
+
+        private void BtnFind_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Text Documents(*.txt)|*.txt", ValidateNames = true, Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string[] lines = System.IO.File.ReadAllLines(ofd.FileName);
+                    LinkedList<string> lists = new LinkedList<string>(lines);
+                    /*foreach (string s in lines)
+                    {
+                        lists.AddLast(s);   //them dan ve sau
+                    }*/
+                    Text = String.Concat(lists); //Chuyen 'lists' tu dang string[] --> string
+                    xf.TextBoxSach.Text = Text;
+                    string temp = "5011      Sức bền vật liệu                   68000     5 ";
+                    LinkedListNode<String> findnode = lists.Find(temp);
+                    if (findnode != null)
+                    {
+                        MessageBox.Show("Node này đã được tìm thấy trong file, xem tại TextboxReview ", "Thông báo");
+                        TextBoxReview.Text = findnode.Value;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Không tìm thấy", "Lỗi");
+                    }
+                    /*
+                    Text = String.Join(" ", lists);
+                    TextBoxReview.Text = String.Join(" ", lists);
+                    System.IO.File.WriteAllText(@"C:\Users\admin\Desktop\Sachupdate.txt", Text);*/
+                }
+            }
+        }
+
+        private void BtnResetReview_Click(object sender, EventArgs e)
+        {
+            TextBoxReview.Clear();
+        }
+    }
+}
